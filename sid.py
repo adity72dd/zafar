c0e0a04dd47d0add4373a56d6b4a88dae3e5d996
adity72dd <mmunnabot7@gmail.com>
2025-02-10 23:56:22 +0530

    Update sid.py

--- sid.py
+++ sid.py
@@ -10,7 +10,7 @@ from config import BOT_TOKEN, ADMIN_IDS, OWNER_USERNAME
 USER_FILE = "users.json"
 DEFAULT_THREADS = 1490
 DEFAULT_PACKET = 9
-DEFAULT_DURATION = 150  # Default attack duration
+DEFAULT_DURATION = 180  # Default attack duration
 BAN_DURATION = 1000  # Ban duration (10 mi22nutes)
 
 users = {}
